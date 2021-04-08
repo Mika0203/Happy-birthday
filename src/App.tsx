84d02c3846a3385356354b985b712f0f854e93bb
Mik_a <ilemik@youcanstar.com>
2021-04-08 17:27:26 +0900

    add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Header } from './components/Header';
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import CalendarPage from './page/CalendarPage';
+import BoardPage from './page/BoardPage';
+import { BirthdayData } from './interface';
+import { GlobalStyle } from './globalstyle';
+
+import api from './api';
+
+enum ViewType {
+  CALENDAR,
+  BOARD
+};
+
+interface ButtonProps {
+  Label: string;
+  Type: ViewType;
+};
+
+const buttonList: ButtonProps[] = [
+  {
+    Label: '달력',
+    Type: ViewType.CALENDAR,
+  },
+  {
+    Label: '게시판',
+    Type: ViewType.BOARD
+  }
+];
 
 function App() {
+  const [currentType, setCurrentType] = useState<ViewType>(ViewType.CALENDAR);
+  const [birthData, setBirthData] = useState<BirthdayData[]>([]);
+
+  useEffect(() => {
+    api.getBirthData().then(res => {
+      const birthdayData: BirthdayData[] = res.data.map((birth: Array<string>) => {
+            const data: BirthdayData = {
+              name: birth[0],
+              nickname: birth[1],
+              birthday: birth[2],
+              type: birth[3],
+              realBirthday: birth[4]
+            };
+            return data;
+          });
+          setBirthData(birthdayData);
+    });
+  }, []);
+
+  const makeButton = (): JSX.Element[] => {
+    return buttonList.map(button =>
+      <Button onClick={() => setCurrentType(button.Type)}>
+        {button.Label}
+      </Button>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <GlobalStyle />
+      <Header buttons={makeButton()} />
+      {
+        currentType === ViewType.CALENDAR
+          ? <CalendarPage birthData={birthData} />
+          : <BoardPage birthData={birthData} />
+      }
+    </Container>
   );
 }
+
+const Container = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  color: white;
+  outline: 0px;
+  padding : 0px;
+  height: 100%;
+  width: 100px;
+  margin : 0px;
+  background-color: unset;
+  &:hover{
+    background-color: rgba(0,0,0,0.1);
+  }
+`;
 
 export default App;
---
+++ src/globalstyle.tsx
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from 'styled-components';
+import font from './asset/font.ttf';
+
+export const GlobalStyle = createGlobalStyle`
+  @font-face{
+    src : url(${font});
+    font-family: 'Maple';
+  }
+
+  body{
+    font-family: 'Maple';
+    margin: 0px;
+  }
+
+  button{
+    border-radius: 5px;
+    border: 0px;
+    margin : 5px;
+    font-family: 'Maple';
+    cursor: pointer;
+  }
+
+  .mainCal {
+    max-width: 700px;
+    width: 100% !important;
+  }
+
+  .maintile {
+    text-align: left !important;
+    border: 1px solid #00000031 !important;
+    border-radius: 0px;
+    height: 7em;
+    margin-top: 5px;
+    display: grid;
+  }
+`;
+
