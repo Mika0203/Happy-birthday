3af553e7d677db5b167f52f9cc6aa644c8215986
Mik_a <ilemik@youcanstar.com>
2021-04-09 16:07:30 +0900

    update api

--- src/api.tsx
+++ src/api.tsx
@@ -1,9 +1,28 @@
 import axios from 'axios';
 
+const url = `http://${window.location.hostname}:3001`;
+
 const api = {
-    getBirthData : async () => {
-        return axios.get('http://' + window.location.hostname + ':3001/getData');
-    }
+    getBirthData: async () => {
+        return axios.get(`${url}/birth-data`);
+    },
+
+    posting: async (data: any) => {
+        return axios.post(`${url}/posting`, data , {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
+    },
+
+    getPost: async (dir: number) => {
+        return axios.get(`${url}/post/${dir.toString()}`);
+    },
+
+    getPostList: async () => {
+        const res = await axios.get(`${url}/post-list`);;
+        return res.data.data;
+    },
 };
 
 export default api;
